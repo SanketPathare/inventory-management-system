9d3dd9be4fa1353e1d3818953fe5179c904eaad0
SanketPathare <sanketpathare8808@gmail.com>
2024-12-19 12:48:17 +0530

    first commit

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { loginSignup } from "@/actions/user";
+import FormInput from "@/components/FormInput";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { toast } from "@/components/ui/use-toast";
+import Link from "next/link";
+import React, { useOptimistic } from "react";
+
+const Login = () => {
+  const [loading, setLoading] = useOptimistic(false);
+
+  const handleSubmit = async (formData: FormData) => {
+    setLoading(true);
+    const res = await loginSignup(formData, true);
+    if (res?.error) {
+      toast({ title: res?.error });
+    }
+    setLoading(false);
+  };
+  return (
+    <div className="grid place-content-center min-h-screen bg-gray-100">
+      <div className="flex flex-col justify-center gap-5 items-center py-10 w-[450px] shadow-lg rounded-lg bg-white">
+        <h1 className="text-center font-bold text-4xl">Login</h1>
+        <form action={handleSubmit} className="w-full px-5">
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="Enter the email"
+            label="Email"
+          />
+          <FormInput
+            name="password"
+            type="password"
+            placeholder="Enter the password"
+            label="Enter Password"
+          />
+          <Button
+            type="submit"
+            className={`${loading && "disabled cursor-not-allowed"
+              } w-full bg-green-500`}
+          >
+            {loading ? "loading..." : "Login"}
+          </Button>
+        </form>
+        <div className="flex gap-2 ">
+          <h1 className="">
+            Don{"'"}t have and account?
+          </h1>
+          <Link
+            href="/signup"
+            className="text-center cursor-pointer "
+          >
+            <span className="text-green-500 font-bold">
+              Sign Up
+            </span>
+          </Link>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/AuthDesign.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+const AuthDesign = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[90vh] bg-gray-50">
+        <h2 className="text-2xl font-semibold  text-center mb-6">
+          Welcome to   Inventory Management System
+        </h2>
+    </div>
+  );
+};
+
+export default AuthDesign;
